9f6138ff939d2c87743934d0b539909d56c257ab
sergei270791 <sergei.calle.c@uni.pe>
2023-11-06 21:46:51 -0500

    Añadiendo Material UI

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -1,53 +1,63 @@
 import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
 
 const Profile = () => {
   const columnStyle = {
     display: 'flex',
-    width: '80%', 
-    height: '80vh', 
-    justifyContent: 'space-around',
+    width: '50%',
+    flexDirection: 'column',
+    alignItems: 'center',
   };
 
   const roundedBoxStyle = {
-    width: '90%', 
+    width: '90%',
     border: '1px solid #ccc',
     borderRadius: '10px',
     padding: '10px',
+    marginBottom: '20px',
   };
 
+  const avatarStyle = {
+    maxWidth: 150,
+  };
+  
+
   return (
-    <div style={columnStyle} className="profile-container">
-      <div className="column" style={columnStyle}>
-        <div style={roundedBoxStyle} className="user-info">
-          <div className="user-avatar">
-            {/* Aquí puedes mostrar la foto del usuario */}
-            <img src="https://www.munichurcampa.gob.pe/wp-content/uploads/2023/01/1665282759496710_c5_720x720.jpeg" 
-            alt="Avatar" 
-            style={{
-					maxWidth: 150,
-				}} 
+    <Box display="flex" width="80%" height="80vh" justifyContent="space-around" className="profile-container">
+      <Box className="column" style={columnStyle}>
+        <Box style={roundedBoxStyle} className="user-info">
+          <Box className="user-avatar">
+            <img
+              src="https://www.munichurcampa.gob.pe/wp-content/uploads/2023/01/1665282759496710_c5_720x720.jpeg"
+              alt="Avatar"
+              style={avatarStyle}
             />
-          </div>
-          <div className="user-details">
-            <p>User: Nombre del usuario</p>
-            <p>Email: correo@ejemplo.com</p>
-          </div>
-          <button>Editar</button>
-        </div>
-      </div>
-      <div className="column" style={columnStyle}>
-        <div style={roundedBoxStyle} className="user-preferences">
-          <h2>Mis Preferencias</h2>
+          </Box>
+          <Box className="user-details">
+            <Typography variant="body1">User: Nombre del usuario</Typography>
+            <Typography variant="body1">Email: correo@ejemplo.com</Typography>
+          </Box>
+          <Button variant="contained" color="primary">
+            Editar
+          </Button>
+        </Box>
+      </Box>
+      <Box className="column" style={columnStyle}>
+        <Box style={roundedBoxStyle} className="user-preferences">
+          <Typography variant="h4">Mis Preferencias</Typography>
           <ul>
             <li>Opción 1</li>
             <li>Opción 2</li>
             <li>Opción 3</li>
           </ul>
-          <button>Actualizar</button>
-        </div>
-      </div>
-    </div>
+          <Button variant="contained" color="primary">
+            Actualizar
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
 export default Profile;
+
