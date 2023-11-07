286a23a7e21121b4989fa45b9c9a5f2166b44c75
sergei270791 <sergei.calle.c@uni.pe>
2023-11-07 09:21:08 -0500

    Primer commit de Lenguajes

--- src/views/Languages/index.js
+++ src/views/Languages/index.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { Box, Button } from '@mui/material';
 
 const Languages = () => {
-    return (
-        <div>Languages</div>
-    )
-}
+  const languages = [
+    {
+      id: 1,
+      name: 'Python',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/800px-Python-logo-notext.svg.png',
+    },
+    {
+      id: 2,
+      name: 'JavaScript',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+    },
+    // Agrega más lenguajes según sea necesario
+  ];
 
-export default Languages
+  const languageBoxStyle = {
+    width: '250px', // Ajusta el ancho de cada caja
+    border: '1px solid #ccc',
+    borderRadius: '10px',
+    padding: '10px',
+    margin: '10px',
+  };
+
+  const imageStyle = {
+    width: '200px',
+    height: '200px',
+  };
+
+  const buttonStyle = {
+    margin: '5px',
+    variant: "contained",
+    color: "primary",
+  };
+
+  return (
+    <div>
+      <h2>¿Qué lenguaje deseas aprender?</h2>
+      <Box display="flex" flexWrap="wrap">
+        {languages.map((language) => (
+          <Box key={language.id} style={languageBoxStyle}>
+            <img
+              src={language.image}
+              alt={`Lenguaje: ${language.name}`}
+              style={imageStyle}
+            />
+            <Button style={buttonStyle}>
+              Ver más
+            </Button>
+          </Box>
+        ))}
+      </Box>
+    </div>
+  );
+};
+
+export default Languages;
+
