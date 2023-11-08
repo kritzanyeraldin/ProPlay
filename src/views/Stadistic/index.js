60ec3de1859dc7f333b2e7251330f93ff2c57b0a
sergei270791 <sergei.calle.c@uni.pe>
2023-11-07 23:29:08 -0500

    Primer commit stadistic

--- src/views/Stadistic/index.js
+++ src/views/Stadistic/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
 
 const Stadistic = () => {
-    return (
-        <div>Stadistic</div>
-    )
-}
+  return (
+    <div>
+      <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column">
+        <div>
+          <Typography variant="h4">Rendimiento por mes</Typography>
+          <img src="notas_por_mes.png" alt="Rendimiento por mes" />
+        </div>
+        <div>
+          <Typography variant="h4">Horas estudiadas por lenguaje</Typography>
+          <img src="tiempo_por_lenguaje" alt="Horas estudiadas por lenguaje" />
+        </div>
+      </Box>
+    </div>
+  );
+};
 
-export default Stadistic
+export default Stadistic;
+
