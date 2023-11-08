55863a584ea4005d95fd1489fb10bdeb119d7b1e
Kritzan Yeraldin Sullca <55310090+kritzanyeraldin@users.noreply.github.com>
2023-11-08 13:08:11 -0500

    error

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { addCourse } from './views/Course/components'
 
 function App() {
 	return (
-		<Box height='100vh'>
+		<>
 			<Routes>
 				<Route
 					path='/'
@@ -69,4 +69,4 @@ function App() {
 }
 
 export default App
-export default App
+
