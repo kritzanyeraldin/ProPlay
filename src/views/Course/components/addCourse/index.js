6f5149590f9b149d181f9958af844d398f068a8a
sergei270791 <sergei.calle.c@uni.pe>
2023-11-22 10:38:17 -0500

    Forma provisional de añadir un curso

--- src/views/Course/components/addCourse/index.js
+++ src/views/Course/components/addCourse/index.js
@@ -1,44 +1,96 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from 'react';
+import { TextField, Button, Select, MenuItem, InputLabel, FormControl, Typography } from '@mui/material';
 
-export const addCourse = () => {
+function AddCourse() {
+  const [courseData, setCourseData] = useState({
+    image: null,
+    name: '',
+    level: '',
+    description: '',
+    topics: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setCourseData({
+      ...courseData,
+      [name]: value,
+    });
+  };
+
+  const handleImageChange = (event) => {
+    const imageFile = event.target.files[0];
+    setCourseData({
+      ...courseData,
+      image: imageFile,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Lógica para manejar/enviar los datos del curso
+    console.log(courseData);
+  };
+
   return (
-    <>
-      <Box
-        bgcolor="grey.200"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        gap={5}
-        py={2}
-      >
-        <Box>
-          <Typography>Create Course</Typography>
-        </Box>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "40ch" },
-          }}
-          noValidate
-          autoComplete="off"
-          display="flex"
-          flexDirection="column"
-          gap={2}
+    <form onSubmit={handleSubmit} encType="multipart/form-data">
+      <Typography variant="h6">Añadir Curso</Typography>
+      <div>
+        <input
+          type="file"
+          accept="image/*"
+          name="image"
+          onChange={handleImageChange}
+        />
+      </div>
+      <TextField
+        label="Nombre del Curso"
+        name="name"
+        value={courseData.name}
+        onChange={handleInputChange}
+        fullWidth
+        required
+        margin="normal"
+      />
+      <FormControl fullWidth required margin="normal">
+        <InputLabel id="level-select-label">Nivel</InputLabel>
+        <Select
+          labelId="level-select-label"
+          id="level-select"
+          name="level"
+          value={courseData.level}
+          onChange={handleInputChange}
         >
-          <TextField label="Name" color="primary" focused />
-          <TextField
-            sx={{ height: "10ch" }}
-            label="Description"
-            color="primary"
-            focused
-          />
-        </Box>
-
-        <Button variant="contained">Siguiente</Button>
-      </Box>
-    </>
+          <MenuItem value="basico">Básico</MenuItem>
+          <MenuItem value="intermedio">Intermedio</MenuItem>
+          <MenuItem value="avanzado">Avanzado</MenuItem>
+        </Select>
+      </FormControl>
+      <TextField
+        label="Descripción"
+        name="description"
+        value={courseData.description}
+        onChange={handleInputChange}
+        fullWidth
+        required
+        margin="normal"
+        multiline
+        rows={4}
+      />
+      <TextField
+        label="Temas (separados por comas)"
+        name="topics"
+        value={courseData.topics}
+        onChange={handleInputChange}
+        fullWidth
+        required
+        margin="normal"
+      />
+      <Button variant="contained" color="primary" type="submit">
+        Agregar Curso
+      </Button>
+    </form>
   );
-};
+}
 
-export default addCourse;
+export default AddCourse;
